eaaed9da43a1a52e4c8b6e2ea2fa483370e6e802
Erdi Köse <me@erdkse.com>
2019-08-11 23:57:39 +0300

    sckeleton

---
+++ src/components/main/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="main-footer">
+    <strong>
+      Copyright &copy; 2014-2019
+      <a href="http://adminlte.io">AdminLTE.io</a>.
+    </strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.0-beta.2
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+</style>
