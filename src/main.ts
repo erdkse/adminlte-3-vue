1534a5c23eb0ba7eef8b2e974355121345726dad
Erdi Köse <me@erdkse.com>
2021-09-19 22:23:16 +0300

    feat: Change facebook App id

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ library.add(faLock, faEnvelope, faFacebook, faGooglePlus);
 Gatekeeper.configure('de378d9c-38c8-42c1-b961-9e4fa92d6a5e', {
     googleClientID:
         '816324818723-e2hokn0pvjgkf8jcks6quido903ukeri.apps.googleusercontent.com',
-    facebookAppID: '489915919118075'
+    facebookAppID: '972653536647742'
 });
 
 const options: PluginOptions = {};
