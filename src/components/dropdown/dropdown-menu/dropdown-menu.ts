11285bb131efb80d54c16df1216b22735d27a4bb
Erdi Köse <me@erdkse.com>
2021-09-25 13:09:26 +0300

    chore: Additional fixes

--- src/components/dropdown/dropdown-menu/dropdown-menu.ts
+++ src/components/dropdown/dropdown-menu/dropdown-menu.ts
@@ -12,20 +12,22 @@ export default class DropdownMenu extends Vue {
 
     get fixStyles(): any {
         if (this.dropdownMenuElement) {
-            const windowOffsetWidth =
-                document.getElementById('app').offsetWidth;
-            const offsetLeft = this.dropdownMenuElement.offsetLeft || -230;
-            const elementWidth = this.dropdownMenuElement.offsetWidth || 280;
-            const visiblePart = windowOffsetWidth - offsetLeft;
+            const windowWidth = document.getElementById('app').offsetWidth;
+            const offsetLeft = this.dropdownMenuElement.offsetLeft;
+            const offsetWidth = this.dropdownMenuElement.offsetWidth;
+            const visiblePart = windowWidth - Math.abs(offsetLeft);
 
+            console.log('windowWidth', windowWidth);
             console.log('offsetLeft', offsetLeft);
-            console.log('elementWidth', elementWidth);
+            console.log('offsetWidth', offsetWidth);
             console.log('visiblePart', visiblePart);
-            console.log('windowOffsetWidth', windowOffsetWidth);
 
             if (offsetLeft < 0) {
-                return {left: 'inherit', right: `${offsetLeft + 90}px`};
-            } else if (visiblePart < elementWidth) {
+                return {
+                    left: 'inherit',
+                    right: `${offsetLeft - 5}px`
+                };
+            } else if (visiblePart < offsetWidth) {
                 return {left: 'inherit', right: `0px`};
             }
             return {left: 'inherit', right: `0px`};
