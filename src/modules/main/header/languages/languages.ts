757fc53817823c2164a27e1d4a2dba042d61229d
Erdi Köse <me@erdkse.com>
2021-09-26 07:57:14 +0300

    feat: Make language dropdown functional

--- src/modules/main/header/languages/languages.ts
+++ src/modules/main/header/languages/languages.ts
@@ -8,9 +8,29 @@ import Dropdown from '@/components/dropdown/dropdown.vue';
     }
 })
 export default class Languages extends Vue {
+    public selectedLanguage: string = null;
+
+    public mounted() {
+        this.selectedLanguage = this.$i18n.locale;
+    }
+
+    get flagIcon() {
+        if (this.selectedLanguage === 'de') {
+            return 'flag-icon-de';
+        }
+        if (this.selectedLanguage === 'fr') {
+            return 'flag-icon-fr';
+        }
+        if (this.selectedLanguage === 'es') {
+            return 'flag-icon-es';
+        }
+        return 'flag-icon-us';
+    }
+
     public changeLanguage(langCode: string) {
         if (this.$i18n.locale !== langCode) {
             this.$i18n.locale = langCode;
+            this.selectedLanguage = langCode;
         }
     }
 }
--- src/modules/main/header/languages/languages.vue
+++ src/modules/main/header/languages/languages.vue
@@ -1,3 +1,3 @@
 <template src="./languages.html"></template>
 <script src="./languages.ts" lang="ts"></script>
-<style src="./languages.scss" lang="scss"></style>
+<style src="./languages.scss" lang="scss" scoped></style>
