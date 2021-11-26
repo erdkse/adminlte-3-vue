cc2395ec7f003a9d001983275acfc0da0ee6c4de
Erdi Köse <me@erdkse.com>
2021-11-26 23:17:06 +0300

    feat: update checkbox component

--- src/components/checkbox/checkbox.ts
+++ src/components/checkbox/checkbox.ts
@@ -4,13 +4,18 @@ import {v4 as uuidv4} from 'uuid';
 @Options({
     name: 'app-checkbox',
     props: {
-        modelValue: Boolean
+        modelValue: Boolean,
+        type: {
+            type: String,
+            default: 'default'
+        }
     },
     emits: ['update:modelValue']
 })
 export default class Checkbox extends Vue {
     private ID: string = uuidv4();
     private modelValue: boolean;
+    private type: string;
 
     public onValueChange(event: any) {
         this.$emit('update:modelValue', event.target.checked);
