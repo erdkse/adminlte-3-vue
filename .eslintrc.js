29cc473e521354d171c2726d26b4c1d75d85a8c2
Erdi Köse <me@erdkse.com>
2022-09-05 00:21:37 +0300

    chore: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
         'plugin:vue/vue3-essential',
         'eslint:recommended',
         '@vue/typescript/recommended',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint'
+        '@vue/prettier'
     ],
     parserOptions: {
         ecmaVersion: 2020
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const {defineConfig} = require('@vue/cli-service');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = defineConfig({
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [new NodePolyfillPlugin()],
+        resolve: {
+            fallback: {
+                fs: false
+            }
+        }
+    }
+});
