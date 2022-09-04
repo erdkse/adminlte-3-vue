300a036f03e721d5f573f46479b016e3ec052b50
Erdi Köse <me@erdkse.com>
2022-09-04 21:47:37 +0300

    chore: add pf-dropdown

--- src/modules/main/header/messages/messages.ts
+++ src/modules/main/header/messages/messages.ts
@@ -1,11 +1,10 @@
 import {Options, Vue} from 'vue-class-component';
-import Dropdown from '@/components/dropdown/dropdown.vue';
-import {PfImage} from '@profabric/vue-components';
+import {PfDropdown, PfImage} from '@profabric/vue-components';
 
 @Options({
     name: 'messages-dropdown',
     components: {
-        'app-dropdown': Dropdown,
+        'pf-dropdown': PfDropdown,
         'pf-image': PfImage
     }
 })
--- src/modules/main/header/user/user.ts
+++ src/modules/main/header/user/user.ts
@@ -1,13 +1,12 @@
 import {IUser} from '@/interfaces/user';
 import {Options, Vue} from 'vue-class-component';
-import Dropdown from '@/components/dropdown/dropdown.vue';
 import {DateTime} from 'luxon';
-import {PfImage} from '@profabric/vue-components';
+import {PfDropdown, PfImage} from '@profabric/vue-components';
 
 @Options({
     name: 'user-dropdown',
     components: {
-        'app-dropdown': Dropdown,
+        'pf-dropdown': PfDropdown,
         'pf-image': PfImage
     }
 })
@@ -21,6 +20,11 @@ export default class User extends Vue {
     }
 
     get readableCreatedAtDate() {
-        return DateTime.fromISO(this.user.createdAt).toFormat('dd LLLL yyyy');
+        if (this.user) {
+            return DateTime.fromISO(this.user.createdAt).toFormat(
+                'dd LLLL yyyy'
+            );
+        }
+        return '';
     }
 }
