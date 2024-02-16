42f53e124a003b97e6e81510bc084a24b2338f09
Erdi Köse <me@erdkse.com>
2024-02-16 11:19:33 +0300

    chore: fix json syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,20 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 const viteEnv = {};
-Object.keys(process.env).forEach((key) => {
-    if (key.startsWith(`VITE_`)) {
-        viteEnv[`import.meta.env.${key}`] = process.env[key];
-    }
-});
 
 // https://vitejs.dev/config/
 export default ({mode}) => {
     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    Object.keys(process.env).forEach((key) => {
+        if (key.startsWith(`VITE_`)) {
+            viteEnv[`import.meta.env.${key}`] = JSON.stringify(
+                process.env[key]
+            );
+        }
+    });
+
+    console.log('viteEnv', viteEnv);
 
     return defineConfig({
         mode: 'development',
